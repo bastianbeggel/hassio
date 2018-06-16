fafc851debbd04db3477597f9a43de7e0d2ea510
Bastian Beggel <bastian.beggel@gmail.com>
2018-06-16 07:27:09 +0000

    update

--- scripts/tv_off.sh
+++ scripts/tv_off.sh
@@ -2,13 +2,14 @@
 #scipt to turn panasonic off.
 #only execute toggle command if tv is on
 
-VAR=`curl -s -X GET --user-agent "Panasonic Android VR-CP UPnP/2.0" http://192.168.1.20:55000/pac/ddd.xml -i`
+VAR=`curl -s -X GET --user-agent "Panasonic Android VR-CP UPnP/2.0" http://192.168.178.26:55000/pac/ddd.xml -i`
 size=${#VAR}
 
 exit_status=$?
 if [ $size -gt 100 ]; then
     echo "tv_is_on:$size"
     nodejs /home/pi/viera.js/tv_power.js
+#    nodejs /home/homeassistant/.homeassistant/scripts/tv_power.js
 else
     echo "tv_is_off:$size"
 #    nodejs /home/pi/viera.js/tv_power.js
--- scripts/tv_on.sh
+++ scripts/tv_on.sh
@@ -2,7 +2,7 @@
 #scipt to turn panasonic on.
 #only execute toggle command if tv is off
 
-VAR=`curl -s -X GET --user-agent "Panasonic Android VR-CP UPnP/2.0" http://192.168.1.20:55000/pac/ddd.xml -i`
+VAR=`curl -s -X GET --user-agent "Panasonic Android VR-CP UPnP/2.0" http://192.168.178.26:55000/pac/ddd.xml -i`
 size=${#VAR}
 
 exit_status=$?
@@ -10,7 +10,9 @@ if [ $size -gt 100 ]; then
     echo "tv_is_on:$size"
 else
     echo "tv_is_off:$size"
+#    nodejs /home/homeassistant/.homeassistant/scripts/tv_power.js
     nodejs /home/pi/viera.js/tv_power.js
+
 fi
 exit 0
 
