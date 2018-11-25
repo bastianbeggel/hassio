a3d711cd29783be324aab44d5def011519a2134e
Bastian Beggel <bastian.beggel@gmail.com>
2018-11-25 19:05:41 +0100

    update

--- scripts/tv_power.js
+++ scripts/tv_power.js
@@ -1,37 +1,6 @@
 var Viera = require('./viera');
 
-// create instance of module
 var tv = new Viera('192.168.178.50');
 
-// get volume value
-//tv.getVolume(function(data) {
-//    console.log('current volume: '+data);
-//});
-
-
-
-// set volume
-//tv.setVolume(20);
-
-// get mute value
-//tv.getMute(function(data) {
-//    console.log('mute: ' + data);
-//});
-
-// set mute
-//tv.setMute(true);
-
-// send command
-//tv.sendCommand("menu");
-
-// you can also chain multiple methods
-//tv.sendCommand("apps")
-//  .sendCommand("down")
- // .sendCommand("enter")
- // .setVolume(25);
-
-//tv.sendCommand("menu").sendCommand("right").sendCommand("enter");
-
-//tv.sendCommand("NRC_POWER-ONOFF");
 tv.sendCommand("power");
 
